247c1aa1e882451fc101066c146d93340f5bf432
iloktionov <iloktionov@skbkontur.ru>
2018-09-24 16:55:08 +0500

    + TimeBudget

---
+++ Vostok.Commons.Time.Tests/TimeBudget_Tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Commons.Testing;
+
+namespace Vostok.Commons.Time.Tests
+{
+    [TestFixture]
+    internal class TimeBudget_Tests
+    {
+        [Test]
+        public void Infinite_budget_should_not_be_expired_and_have_a_lot_of_remaining_time()
+        {
+            TimeBudget.Infinite.HasExpired.Should().BeFalse();
+            TimeBudget.Infinite.Remaining.Should().Be(TimeSpan.MaxValue);
+        }
+
+        [Test]
+        public void Expired_budget_should_be_expired_and_have_zero_remaining_time()
+        {
+            TimeBudget.Expired.HasExpired.Should().BeTrue();
+            TimeBudget.Expired.Remaining.Should().Be(TimeSpan.Zero);
+        }
+
+        [Test]
+        public void StartNew_should_produce_a_running_budget()
+        {
+            var budget = TimeBudget.StartNew(10.Seconds());
+
+            var remainingBefore = budget.Remaining;
+
+            Thread.Sleep(1);
+
+            budget.Remaining.Should().BeLessThan(remainingBefore);
+        }
+
+        [Test]
+        public void StartNew_should_produce_a_budget_that_will_eventually_expire()
+        {
+            var budget = TimeBudget.StartNew(250.Milliseconds());
+
+            budget.HasExpired.Should().BeFalse();
+
+            new Action(() => budget.HasExpired.Should().BeFalse())
+                .ShouldPassIn(10.Seconds());
+        }
+
+        [Test]
+        public void TryAcquire_should_be_limited_by_remaining_time()
+        {
+            var budget = TimeBudget.StartNew(250.Milliseconds());
+
+            budget.TryAcquire(1.Seconds()).Should().BeLessOrEqualTo(250.Milliseconds());
+        }
+    }
+}
---
+++ Vostok.Commons.Time/TimeBudget.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using JetBrains.Annotations;
+
+namespace Vostok.Commons.Time
+{
+    [PublicAPI]
+    internal class TimeBudget
+    {
+        public static readonly TimeBudget Infinite = new TimeBudget(TimeSpan.MaxValue, TimeSpan.Zero);
+
+        public static readonly TimeBudget Expired = new TimeBudget(TimeSpan.Zero, TimeSpan.Zero);
+
+        private static readonly TimeSpan DefaultPrecision = TimeSpan.FromMilliseconds(5);
+
+        private readonly Stopwatch stopwatch;
+
+        private TimeBudget(TimeSpan total, TimeSpan precision)
+        {
+            if (total < TimeSpan.Zero)
+                throw new ArgumentOutOfRangeException(nameof(total), $"Negative time budget value: '{total}'");
+
+            if (precision < TimeSpan.Zero)
+                throw new ArgumentOutOfRangeException(nameof(precision), $"Negative precision value: '{precision}'");
+
+            Total = total;
+            Precision = precision;
+
+            stopwatch = new Stopwatch();
+        }
+
+        public static TimeBudget StartNew(TimeSpan budget)
+        {
+            return StartNew(budget, DefaultPrecision);
+        }
+
+        public static TimeBudget StartNew(TimeSpan budget, TimeSpan precision)
+        {
+            var result = new TimeBudget(budget, precision);
+
+            result.stopwatch.Start();
+
+            return result;
+        }
+
+        public TimeSpan Total { get; }
+
+        public TimeSpan Precision { get; }
+
+        public TimeSpan Elapsed => stopwatch.Elapsed;
+
+        public TimeSpan Remaining
+        {
+            get
+            {
+                var remaining = Total - Elapsed - Precision;
+
+                return remaining >= TimeSpan.Zero ? remaining : TimeSpan.Zero;
+            }
+        }
+
+        public bool HasExpired => Remaining == TimeSpan.Zero;
+
+        public TimeSpan TryAcquire(TimeSpan needed)
+        {
+            var remaining = Remaining;
+
+            return needed <= remaining ? needed : remaining;
+        }
+    }
+}
